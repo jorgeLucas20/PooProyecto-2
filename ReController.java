057eda3e7d8dc3e303041ed451e8770106d1e695
Rvjacome <87673050+Rvjacome@users.noreply.github.com>
2021-08-31 10:31:22 -0500

    Update ReController.java

--- ReController.java
+++ ReController.java
@@ -1,23 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.projectgame;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.ProcessBuilder.Redirect.Type;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
-import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
@@ -25,11 +31,10 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 
 /**
  * FXML Controller class
@@ -39,396 +44,295 @@ import javafx.scene.shape.Rectangle;
 public class ReController implements Initializable {
 
     @FXML
-    private Label registrados;
-    
-    @FXML
-    private Label presupuesto;
-    @FXML
-    private GridPane Ciudad;
-    @FXML
-    private Button up;
-    @FXML
-    private Button Right;
-    @FXML
-    private Button Down;
-    @FXML
-    private Button Left;
-    @FXML
     private AnchorPane Colorz;
     @FXML
-    private VBox edit;
+    private Label registrados;
+    @FXML
+    private GridPane Mapa;
+    @FXML
+    private Label Tiempo;
+    @FXML
+    private Button Guardar;
+    @FXML
+    private ComboBox<Servicio> Servicios;
+    @FXML
+    private Label presupuesto;
+    private int presupuestoInicial;
+    
+    public static ArrayList <Servicio> servicioList = new ArrayList<>();
+    @FXML
+    private Button Aceptar;
+    private int tiempo;
+    
+    
+    
+
+   
 
     /**
      * Initializes the controller class.
      */
-    
-    
-    
-    
-    
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       //rearGridPane();
-        label();
+        registrados.setText(Usuario.user.toString().replace("[", "").replace("]", ""));
+        Tiempo.setText("Tiempo : "+tiempo);
+        leerServicio();
+        Servicios.getItems().addAll(servicioList);
         cambiarFondo();
-        Ciudad.setStyle("-fx-background-color: #536cff");
+        llenarGrid();
+        Aceptar.setOnAction(t->  agregarValor(Servicios.getValue()));
+        Thread t=new Thread(()-> pasoTemp());
+        t.start();
         
         
-        
-        final Circle circulo = new Circle(15, Color.TOMATO);
-        circulo.setEffect(new Lighting());
-        
-        
-        
-        Ciudad.add(circulo, 0,0);
-        GridPane.setHalignment(circulo, HPos.CENTER);
-        
-        up.setOnAction(t -> arriba(circulo));
-        Down.setOnAction(t -> abajo(circulo));
-        Left.setOnAction(t -> izquierda(circulo));
-        Right.setOnAction(t -> derecha(circulo));
-        
-        
-        tocar();
-        /*
-        Hilo panel = new Hilo(Ciudad, edit); 
-        Thread hilo1 = new Thread(() -> tocar());
-        hilo1.start();
-        
-        */
-    }    
+        //tocar();   
+    } 
     
+    public void agregarValor(Servicio s){
+        String nombre=s.getNombre();
+        String ruta=s.getRuta();
+        int Costomensual=s.getCostoMensual();
+        int Presioconstruccion=s.getPrecioConstruccion();
+        ponerServicio(ruta,Presioconstruccion);
+        
+        
+        
+    }
     
+    public void llenarGrid(){
+        
+        
+        for(int i= 0 ; i<30; i++){
+            for(int j = 0 ; j<18;j++){
+                StackPane qseparado = new StackPane();
+                InputStream input = App.class.getResourceAsStream("Imagenes/grassTile.png");
+                ImageView piso =new ImageView();
+                piso.setImage(new Image(input,25,30, false, false));;
+       
+                int iEditable = i;
+                int jEditable  = j;
+
+                qseparado.getChildren().addAll(piso);
+                
+                Mapa.add(qseparado, i, j);
+
+            }
+                
+        }Mapa.setGridLinesVisible(true);
+    }
     
-    
-    //*****************************
-    public void crearGridPane(){
-        
-        InputStream image = App.class.getResourceAsStream("Normal/grassTile.png");
-        ImageView piso =new ImageView();
-        piso.setImage(new Image(image, 70, 68, false, false));
-        
-     
-        
-        
-        
-        GridPane gp = new GridPane();
-        gp.setHgap(30);
-        gp.setVgap(18); 
-        gp.setPadding(new Insets(10,10,10,10));
-        
+     public void ponerServicio(String ruta,int Presioconstruccion){
         
         
         for(int i= 0 ; i<30; i++){
             for(int j = 0 ; j<18;j++){
                 StackPane qseparado = new StackPane();
                 
-                qseparado.setStyle("-fx-background-color: #536cff");
+       
+                int iEditable = i;
+                int jEditable  = j;
                 
-                
-                gp.add(qseparado, i, j);
-                
-                
+                cambiar(qseparado,ruta,Presioconstruccion);
+         
+                Mapa.add(qseparado, i, j);
+ 
             }
                 
-        }
-            
-        
-        gp.setGridLinesVisible(true);
-        
-        edit.getChildren().add(gp);
-        
-    }
-
-    
-    
-    
-    //************************************
-    public GridPane getCiudad() {
-        return Ciudad;
+        }Mapa.setGridLinesVisible(true);
     }
     
+    public void cambiar(StackPane x,String ruta,int Presioconstruccion){
+        
+        x.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        @Override
+                 public void handle(MouseEvent mouseEvent) {
+                System.out.println("Group!" + mouseEvent.getSource());
+                
+                ButtonType queen = new ButtonType("Construir");
+                ButtonType rook = new ButtonType("Demoler");
+                Alert a = new Alert(AlertType.NONE, "Elige entre: ", queen, rook);
+                a.setTitle("Primero que nada , Buenos dias");
+                
+                a.setResizable(true);
+                a.setContentText("Desea constuir o demoler un servicio?");
+                a.showAndWait().ifPresent(response -> {
+                if (response == queen) {
+             
+                    System.out.println("Yeah");
+                    Alert b = new Alert(AlertType.INFORMATION);
+                    b.setTitle("Va a contruir Yeahh");
+                    b.setContentText("Por favor elige un tipo de construccion a lado del panel. ");
+                    b.showAndWait();
+                    
+                    Aceptar.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                    System.out.println("Group!" + mouseEvent.getSource());
+   
+                    Servicio s = Servicios.getValue();
+                    
+                    String nombre=s.getNombre();
+                        System.out.println(nombre);
+                    
+                    
+                    String ruta=s.getRuta();
+                    int Costomensual=s.getCostoMensual();
+                    int Presioconstruccion=s.getPrecioConstruccion();
+                    
+                    
+                    x.getChildren().clear();
+                    InputStream input = App.class.getResourceAsStream(ruta);
+                    ImageView piso3 =new ImageView();
+                    piso3.setImage(new Image(input,25,30, false, false));
+                    
+                    x.getChildren().add(piso3);
+ 
+                    
+                    }
+                    });
+                      
+                    
+                } else if (response == rook) {
+                    x.getChildren().clear();
+                    
+                    InputStream input = App.class.getResourceAsStream("EXTRAS/gif-muro-07.gif");
+                    ImageView pisoUWU =new ImageView();
+                    pisoUWU.setImage(new Image(input,25,30, false, false));
+                    
+                    x.getChildren().add(pisoUWU);
+                    
+                    
+                 }
+                });
+                
+                InputStream input = App.class.getResourceAsStream(ruta);
+                ImageView image =new ImageView();
+                image.setImage(new Image(input,25,30, false, false));;
+                
+                
+                x.getChildren().add(image);
+                presupuestoInicial-=Presioconstruccion;
+                presupuesto.setText("Presupuesto: "+presupuestoInicial);
+                
+                
+                //Aqui esta mmda se puede crear un menu dicendo tu servicio a poner 
+                
+                }
+                });
+                
+        
+    }
     
     
-    public void tocar(){
+    public void crearServicio(MouseEvent event,Servicio s){
+        InputStream input = App.class.getResourceAsStream(s.getRuta());
+        ImageView image =new ImageView();
+        image.setImage(new Image(input,25,30, false, false));;
+        
         
        
-        
-        
-        System.out.println("Se ejecuta");
-        for(int i= 0 ; i<30; i++){
-            for(int j = 0 ; j<18;j++){
-                InputStream image = App.class.getResourceAsStream("Normal/grassTile.png");
-                ImageView piso =new ImageView();
-                piso.setImage(new Image(image, 35, 34, false, false));
-                
-                
-                
-                StackPane qseparado = new StackPane();
-                qseparado.getChildren().add(piso);
-                
-                
-                int a = i;
-                int b = j;
-                 
-                Ciudad.add(qseparado, a, b);
-                
-                
-                //Armar un thread
-                qseparado.setOnMouseClicked(e -> cont(a,b));
-                
-                
-                
-                
-                
-               
-                
-                
-                
-                
-            }
-                
-        }
-        
+
     }
     
-    
-    public void cont(int columna, int fila){
-        
-        Node x = Pepito(Ciudad, columna, fila);
-        
-        
-        if( x == null){
-            InputStream image = App.class.getResourceAsStream("Normal/grassTile.png");
-            ImageView piso =new ImageView();
-            piso.setImage(new Image(image, 35, 34, false, false));
-        
-            Ciudad.add(piso, columna, fila);
-        }else{
-            
-            InputStream image2 = App.class.getResourceAsStream("EXTRAS/gif-muro-07.gif");
-            ImageView piso2 = new ImageView();
-            piso2.setImage(new Image(image2, 25, 20, false, false));
-            
-            Ciudad.add(piso2, columna, fila);
-        }
-        
-        
-        
-        
-        
-        
-    }
-    
-    
-    
-    private Node Pepito(GridPane gridPane, int col, int row) {
+    private Node Grid(GridPane gridPane, int col, int row) {
     for (Node node : gridPane.getChildren()) {
         if (GridPane.getColumnIndex(node) == col && GridPane.getRowIndex(node) == row) {
             return node;
         }
     }
     return null; 
-}
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //new Rectangle(50,50, Color.BEIGE)
-    
-    
-    
-    
-    
-        
-        
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }
     
     
    
-    
-    //Botones del juego
-    
-    public void arriba(Circle circulo){
-        
-        try{
-            GridPane.setRowIndex(circulo, GridPane. getRowIndex(circulo)-1);
-        }catch(IllegalArgumentException ex){
-            
-            Alert problema = new Alert(Alert.AlertType.INFORMATION);
-            problema.setTitle("Hubo un error en al momento de moverte hacia arriba.");
-            problema.setContentText("Flaco, sabes es bueno ser curioso, pero te pasas.");        
-            problema.showAndWait();
-        }
-        
-        
-    }
-    public void abajo(Circle circulo){
-        try{
-            GridPane.setRowIndex(circulo, GridPane. getRowIndex(circulo)+1);
-        }catch(IllegalArgumentException ex){
-            Alert problema = new Alert(Alert.AlertType.INFORMATION);
-            problema.setTitle("Hubo un error en el momento de moverte hacia abajo");
-            problema.setContentText("Bro, estas fuera de los limites");
-            problema.showAndWait();
-        }
-        
-    }
-    
-    
-    public void izquierda(Circle circulo){
-        try{
-            GridPane.setColumnIndex(circulo, GridPane. getColumnIndex(circulo)-1);
-        }catch(IllegalArgumentException ex){
-            
-            Alert problema = new Alert(Alert.AlertType.INFORMATION);
-            problema.setTitle("Hubo un problema al momento de moverte hacia la izquierda");
-            problema.setContentText("Bro, estas mal");
-            problema.showAndWait();
-            
-        }
-        
-    }
-    public void derecha(Circle circulo){
-        try{
-            GridPane.setColumnIndex(circulo, GridPane. getColumnIndex(circulo)+1);
-        }catch(IllegalArgumentException ex){
-            
-            Alert problema = new Alert(Alert.AlertType.INFORMATION);
-            problema.setTitle("Hubo un problema al momento de moverte hacia la derecha");
-            problema.setContentText("Tas bien bro?");
-            problema.showAndWait();
-            
-            
-        }
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public void label(){
-        
-        registrados.setText(Usuario.user.toString());
-        
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+   
     public void cambiarFondo(){
-        Colorz.setStyle("-fx-background-color: #000000");
+        
+        Colorz.setStyle("-fx-background-color: #ffffff");
         
         
         switch(GameController.nuevoUsuario){
-            case "Modo niño chillon": presupuesto.setText("Presupuesto: 1000");
+            case "Facil":
+                presupuestoInicial=20000;
+                presupuesto.setText("Presupuesto: "+presupuestoInicial);
                 break;
             
-            case "Modo adulto promedio": presupuesto.setText("Presupuesto: 500");
+            case "Medio":
+                presupuestoInicial=10000;
+                presupuesto.setText("Presupuesto: "+presupuestoInicial);
                 break;
                 
-            case "Te cagas bro": presupuesto.setText("Presupuesto: 500");
+            case "Dificil":
+                presupuestoInicial=5000;
+                presupuesto.setText("Presupuesto: "+presupuestoInicial);
                 break;
+        } 
+     
+    }
+    
+    
+    public  void Guardar(){
+        GridPane guardado= Mapa;
+        //Pelicula p=new Pelicula("Harry Poter","15:20");
+        try(ObjectOutputStream s= new ObjectOutputStream(new FileOutputStream("nose.bin"))){
+            s.writeObject(guardado);
+            System.out.println("Se serializo con exito");
         }
-        
-        
-        
-    }
-
-    private void Construir(MouseEvent event) {
-        
-        System.out.println("HOla");
-        
-        
+        catch(IOException e){
+            System.out.println("Error al serializar"+e);
+        }
+        System.out.println("*****");
     }
     
     
-    public void imagenes(){
-        
-        Label info = new Label("Has elegido una casilla por favor seleciona ");
-        
-        
+    
+    public static void leerServicio(){
+       try{
+       //InputStream input = App.class.getResourceAsStream("archivos/servicios.csv");
+       InputStreamReader lector= new InputStreamReader(App.class.getResourceAsStream("archivos/servicios.txt"));
+       BufferedReader reader= new BufferedReader(lector);
+       String linea;
+       int i = 1;
+       
+       //No se tiene que leer la primera linea
+       while ( (linea=reader.readLine()) != null){
+           
+           if (i!=1){
+               String [] l = linea.split(";");
+               servicioList.add(new Servicio(l[0],l[1],Integer.parseInt(l[2]),Integer.parseInt(l[3])));
+               
+           }else{
+               i++;
+           }
+           
+           
+       }
+       }
+       
+       catch (IOException | NullPointerException ex){
+           ex.printStackTrace();
+           Alert al=new Alert(AlertType.ERROR);  
+           al.setTitle("Error al leer archivo");
+           al.setContentText("no se encuentra el archivo"+ex.toString());
+           al.showAndWait();
+       }
+       
     }
-
     
-    
-    
-    
-    
-    
+    public void pasoTemp(){
+        while(true){
+        
+        try {
+            Platform.runLater(()-> Tiempo.setText("Tiempo : "+tiempo));
+                    Thread.sleep(2000);
+            Thread.sleep(1000);
+            tiempo++;
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        }
+    }
     
     
     
 }
-
