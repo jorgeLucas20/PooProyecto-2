e4fda6a2c7c92f32a9f7d8e2e44c4aabcd247e42
Xavier Cobos <86490191+xxcobos@users.noreply.github.com>
2021-08-31 01:08:53 -0500

    Borrar archivo clonado

--- ProjectGame/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.projectgame {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.projectgame to javafx.fxml;
-    exports com.mycompany.projectgame;
-}
