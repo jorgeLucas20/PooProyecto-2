2989973d1c0f1745728bb0d20b1ec08a754f8807
jorgeLucas20 <85529681+jorgeLucas20@users.noreply.github.com>
2021-08-31 00:37:56 -0500

    Create Servicio.java

---
+++ Servicio.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.projectgame;
+ 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+ 
+/**
+ *
+ * @author jorge
+ */
+public class Servicio {
+     
+     
+     
+    private String nombre;
+    private String ruta;
+    private int precioConstruccion;
+    private int costoMensual;
+ 
+    public Servicio(String nombre, String ruta, int precioConstruccion, int costoMensual) {
+        this.nombre = nombre;
+        this.ruta = ruta;
+        this.precioConstruccion = precioConstruccion;
+        this.costoMensual = costoMensual;
+    }
+ 
+    public String getNombre() {
+        return nombre;
+    }
+ 
+    public String getRuta() {
+        return ruta;
+    }
+ 
+    public int getPrecioConstruccion() {
+        return precioConstruccion;
+    }
+ 
+    public int getCostoMensual() {
+        return costoMensual;
+    }
+ 
+     
+ @Override 
+    public String toString() {
+         
+        return nombre;
+    }
+  
+     
+}
